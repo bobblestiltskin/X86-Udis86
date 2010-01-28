da88976c8dd2cdb8830ba69061d43339e415cce2
Bob Wilkinson <bob@italy.fourtheye.org>
2010-01-28 21:34:32 +0000

    added some comments

--- lib/X86/Udis86.pm
+++ lib/X86/Udis86.pm
@@ -59,6 +59,60 @@ XSLoader::load('X86::Udis86', $VERSION);
 
 # Autoload methods go after =cut, and are processed by the autosplit program.
 
+# struct ud
+# {
+#   int                   (*inp_hook) (struct ud*);
+#   uint8_t               inp_curr;
+#   uint8_t               inp_fill;
+#   FILE*                 inp_file;
+#   uint8_t               inp_ctr;
+#   uint8_t*              inp_buff;
+#   uint8_t*              inp_buff_end;
+#   uint8_t               inp_end;
+#   void                  (*translator)(struct ud*);
+#   uint64_t              insn_offset;
+#   char                  insn_hexcode[32];
+#   char                  insn_buffer[64];
+#   unsigned int          insn_fill;
+#   uint8_t               dis_mode;
+#   uint64_t              pc;
+#   uint8_t               vendor;
+#   struct map_entry*     mapen;
+#   enum ud_mnemonic_code mnemonic;
+#   struct ud_operand     operand[3];
+#   uint8_t               error;
+#   uint8_t               pfx_rex;
+#   uint8_t               pfx_seg;
+#   uint8_t               pfx_opr;
+#   uint8_t               pfx_adr;
+#   uint8_t               pfx_lock;
+#   uint8_t               pfx_rep;
+#   uint8_t               pfx_repe;
+#   uint8_t               pfx_repne;
+#   uint8_t               pfx_insn;
+#   uint8_t               default64;
+#   uint8_t               opr_mode;
+#   uint8_t               adr_mode;
+#   uint8_t               br_far;
+#   uint8_t               br_near;
+#   uint8_t               implicit_addr;
+#   uint8_t               c1;
+#   uint8_t               c2;
+#   uint8_t               c3;
+#   uint8_t               inp_cache[256];
+#   uint8_t               inp_sess[64];
+#   struct ud_itab_entry * itab_entry;
+# };
+
+#sub new {
+#  my $proto = shift;
+#  my $class = ref($proto) || $proto;
+#  my $self = X86::Udis86::_new();
+#  bless $self, $class;
+#
+#  $self;
+#}
+
 1;
 __END__
 # Below is stub documentation for your module. You'd better edit it!
