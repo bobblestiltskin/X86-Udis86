3df396d5741e1f907753f334174849da412cabc8
Bob Wilkinson <bob@fourtheye.org>
2010-03-14 19:35:32 +0000

    add some tests for operands

--- mytest.pl
+++ mytest.pl
@@ -16,6 +16,24 @@ while($ud_obj->disassemble) {
   print join(" ",sprintf("%016x", $ud_obj->insn_off),
        sprintf("%-16x", hex($ud_obj->insn_hex)),
        $ud_obj->insn_asm,"\n");
-
+  print "pfx_rex is ",$ud_obj->pfx_rex,"\n";
+  print "pfx_seg is ",$ud_obj->pfx_seg,"\n";
+  print "pfx_opr is ",$ud_obj->pfx_opr,"\n";
+  print "pfx_adr is ",$ud_obj->pfx_adr,"\n";
+  print "pfx_lock is ",$ud_obj->pfx_lock,"\n";
+  print "pfx_rep is ",$ud_obj->pfx_rep,"\n";
+  print "pfx_repe is ",$ud_obj->pfx_repe,"\n";
+  print "pfx_repne is ",$ud_obj->pfx_repne,"\n";
+  print "pc is ",$ud_obj->pc,"\n";
+  my @operands = $ud_obj->operands;
+use Data::Dumper; print "REF OPS is ",ref @operands," OPS are ",Data::Dumper->Dump([\@operands]);
+  for (my $i=0; $i<3; $i++) {
+    my $operand = X86::Udis86::Operand->new;
+use Data::Dumper; print "REF OP is ",ref $operand," OP is ",Data::Dumper->Dump([$operand]);
+    $operand = $operands[$i];
+use Data::Dumper; print "REF OP is ",ref $operand," OP is ",Data::Dumper->Dump([$operand]);
+#    print "Op $i type is ",$operand->type,"\n";
+#    print "Op $i size is ",$operand->size,"\n";
+  }    
 }
 #close BYTES;
