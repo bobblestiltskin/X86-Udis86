ad792ea748d13aabae15f1be26b0106ae0f68a8f
Bob Wilkinson <bob@fourtheye.org>
2010-03-28 20:36:28 +0100

    tweaked

--- mytest.pl
+++ mytest.pl
@@ -2,10 +2,14 @@
 use strict;
 use X86::Udis86;
 
+use Data::Dumper;
+use Devel::Peek;
+
 #(@ARGV == 1) or die "Need some bytes, $!";
 #open BYTES, $ARGV[0] or die "Can't open ",$ARGV[0], ", $!";
 
-my $bytes = "\x8d\x4c\x24\x04\x83\xe4\xf0\xff\x71\xfc\x55\x89\xe5\x51";
+#my $bytes = "\x8d\x4c\x24\x04\x83\xe4\xf0\xff\x71\xfc\x55\x89\xe5\x51";
+my $bytes = "\x8d\x4c\x24\x04";
 
 my $ud_obj = X86::Udis86->new;
 #$ud_obj->set_input_file(*BYTES);
@@ -26,15 +30,20 @@ while($ud_obj->disassemble) {
   print "pfx_repe is ",$ud_obj->pfx_repe,"\n";
   print "pfx_repne is ",$ud_obj->pfx_repne,"\n";
   print "pc is ",$ud_obj->pc,"\n";
-  my @operands = $ud_obj->operands;
-use Data::Dumper; print "REF OPS is ",ref @operands," OPS are ",Data::Dumper->Dump([\@operands]);
-  for (my $i=0; $i<3; $i++) {
-    my $operand = X86::Udis86::Operand->new;
-use Data::Dumper; print "REF OP is ",ref $operand," OP is ",Data::Dumper->Dump([$operand]);
-    $operand = $operands[$i];
-use Data::Dumper; print "REF OP is ",ref $operand," OP is ",Data::Dumper->Dump([$operand]);
-#    print "Op $i type is ",$operand->type,"\n";
-#    print "Op $i size is ",$operand->size,"\n";
+  my $operands = $ud_obj->operands;
+print "HAVE ",scalar @$operands, " operands\n";
+print "GOT OPS\n";
+
+  my $max = $ud_obj->insn_len < 3 ? $ud_obj->insn_len : 3;
+  for (my $i=0; $i<$max; $i++) {
+    my $operand = $operands->[$i];
+#print "REF OP is ",ref $operand,"\nOP is ",Data::Dumper->Dump([$operand]),"\nDump is ",Dump($operand),"\n";
+    print "Op $i type is ",$operand->{type},"\n";
+    print "Op $i size is ",$operand->{size},"\n";
+    print "Op $i base is ",$operand->{base},"\n";
+    print "Op $i index is ",$operand->{index},"\n";
+    print "Op $i scale is ",$operand->{scale},"\n";
+    print "Op $i offset is ",$operand->{offset},"\n";
   }    
 }
 #close BYTES;
